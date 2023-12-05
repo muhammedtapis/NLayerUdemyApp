d3001c54d986c3949054d85d18234d1c2efb91fc
muhammedtapis <muhammedtapis@gmail.com>
2023-12-05 12:51:57 +0300

    Entity,IGenericRepository,IService,IUnitOfWork interface tanımlamaları

---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    //servis katmanında implemente edilecek repositorydeki metodlarla aynı ama bunların ileride productRepository productService interface
+    //yazdığımızda bu metodların dönüş tipleri vs değişecek.
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<IEnumerable<T>> GetAllAsync(); //IGenericRepositoryden değişik bu mesela collection döncek sorgusuz tüm datayı çekcez.
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        //Burada update remove metodları void olamaz çünkü SaveChanges(); yapacağız veritabanına bu değişiklikleri yansıtacağız.
+        Task UpdateAsync(T entity);
+
+        Task RemoveAsync(T entity);
+
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
---
+++ NLayer.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync(); //implemente ettiğimizde  DBContextin saveChangesAsync(); metoduna karşılık gelecek.
+
+        void Commit(); //implemente ettiğimizde  DBContextin saveChanges(); metoduna karşılık gelecek
+    }
+}
