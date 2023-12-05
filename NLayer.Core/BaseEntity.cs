b8bb6fc6e8f6a44b77344928012c05b1ba5434e7
muhammedtapis <muhammedtapis@gmail.com>
2023-12-05 11:39:42 +0300

    Add project files.

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    //abstractt yapmamızın sebebi bu baseEntityden nesne örneği oluşturulamasın yani soyut yapıyoruz interface gibi
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
